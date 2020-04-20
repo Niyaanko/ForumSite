6739fb5a96d546840009f1be895d2c3de3bbc338
Niyaanko <grovvthpower@gmail.com>
2020-04-20 16:20:45 +0900

    アカウント登録モデルの作成

---
+++ forum/application/models/Register_model.php
@@ -0,0 +1,33 @@
+<?php 
+class Register_model extends CI_model {
+
+    public function __construct()
+    {
+        $this->load->database();
+    }
+
+    public function regist_account()
+    {
+        $nickname = '名無しさん';
+        $data = array(
+            'nickname' => $nickname,
+            'mailaddress' => $this->input->post('mailaddress'),
+            'password' => $this->regist_password_hash($this->input->post('password')),
+            'permission' => 1
+        );
+        return $this->db->insert('users', $data);
+    }
+
+    // 暗号化方法を隠蔽
+    private function regist_password_hash($pass)
+    {
+        return password_hash($pass, PASSWORD_BCRYPT);
+    }
+
+    // パスワードが一致していればTrue 一致していなければFalseを返却
+    private function login_password_verify($pass, $hash)
+    {
+        return password_verify($pass, $hash);
+    }
+
+}
