5e8eb6902f47ee13908821d9cd5149690a2dafc2
Niyaanko <grovvthpower@gmail.com>
2020-04-20 16:38:59 +0900

    ログインページの作成

---
+++ forum/application/views/login_page.php
@@ -0,0 +1,10 @@
+<h2>ログイン</h2>
+
+<?php echo validation_errors(); ?>
+
+<?php echo form_open('login/login'); ?>
+    <h3>メールアドレス<input type="text" name="mailaddress" value="<?php echo set_value('mailaddress'); ?>"></h3>
+    <h3>パスワード<input type="password" name="password"></h3>
+    <input type="submit" name="submit" value="ログイン" />
+</form>
+<p>アカウントをお持ちでない方は<a href=<?php echo site_url("register/regist"); ?>>こちら</a></p>
