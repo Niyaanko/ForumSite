5dfa09a4fd1bbab0ac2a3547756d52aa6b521ca3
Niyaanko <grovvthpower@gmail.com>
2020-04-30 17:48:31 +0900

    パスワード変更ページ作成２

---
+++ forum/application/views/password_page.php
@@ -0,0 +1,27 @@
+<main>
+<h2 class="form_title">パスワード変更</h2>
+    <?php 
+        if(isset($success) && $success === TRUE)
+        { 
+        // パスワード変更に成功した場合 ?>
+        <h3 class="success_msg">パスワードの変更が完了しました。</h3>
+        <a class="maypage_link" href="<?php echo site_url("mypage/mypage")?>">マイページへ</a>
+        <?php 
+        }else{ 
+            // パスワード変更に失敗した場合、メッセージ表示 ?>
+            <h3><?php echo validation_errors(); ?><h3>
+            <?php 
+            if(isset($success) && $success === FALSE){ ?>
+                <h3 class="failure_msg">パスワード変更に失敗しました</h3>
+            <?php 
+            }
+        echo form_open('mypage/pw_change'); ?>
+        <h3 class="password_conf">古いパスワード<input type="password" name="password_conf"></h3>
+        <h3 class="password_new">新しいパスワード<input type="password" name="password_new"></h3>
+        <input class="change_button" type="submit" name="submit" value="変更">
+    </form>
+    <a class="cancel_link" href="<?php echo site_url("mypage/mypage")?>">キャンセル</a>
+
+        <?php 
+        } ?>
+</main>
